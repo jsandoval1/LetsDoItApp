e0b557077e9190d281b66bb0aa38c14dd5001117
John Sandoval <jcsandoval978@gmail.com>
2024-01-24 16:10:39 -0500

    Add Spring Boot annotations and override configure method

--- src/main/java/sandoval/john/letsdoit/LetsdoitApplication.java
+++ src/main/java/sandoval/john/letsdoit/LetsdoitApplication.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// Use the @SpringBootApplication annotation to indicate this is a Spring Boot application
 @SpringBootApplication
+// Extend SpringBootServletInitializer to support WAR file deployment
 public class LetsdoitApplication extends SpringBootServletInitializer {
 
+	// Override the configure method from SpringBootServletInitializer
+	// This method is used when the application is deployed as a WAR file
+	// The sources method tells Spring Boot to start the application with the LetsdoitApplication class
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(LetsdoitApplication.class);
