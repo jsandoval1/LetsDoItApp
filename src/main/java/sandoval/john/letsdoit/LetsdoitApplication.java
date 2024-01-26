4c1b6072edf17fe7f7256ab132445ba2a0941a59
John Sandoval <jcsandoval978@gmail.com>
2024-01-26 14:15:36 -0500

    no longer running as War file

--- src/main/java/sandoval/john/letsdoit/LetsdoitApplication.java
+++ src/main/java/sandoval/john/letsdoit/LetsdoitApplication.java
@@ -2,21 +2,9 @@ package sandoval.john.letsdoit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-// Use the @SpringBootApplication annotation to indicate this is a Spring Boot application
 @SpringBootApplication
-// Extend SpringBootServletInitializer to support WAR file deployment
-public class LetsdoitApplication extends SpringBootServletInitializer {
-
-	// Override the configure method from SpringBootServletInitializer
-	// This method is used when the application is deployed as a WAR file
-	// The sources method tells Spring Boot to start the application with the LetsdoitApplication class
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LetsdoitApplication.class);
-	}
+public class LetsdoitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LetsdoitApplication.class, args);
